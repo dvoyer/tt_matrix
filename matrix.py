5dd80cd0379d3d8adb816a078f42f215e6da0e77
brianvoyer <class2017@Khoshekh.local>
2016-04-26 02:09:19 -0400

    Added to_pitches

--- matrix.py
+++ matrix.py
@@ -2,40 +2,19 @@
 # matrix v2. 20 April 2016
 
 from matrix_tools import *
+import time
 twelvetone = [0,1,2,3,4,5,6,7,8,9,10,11]
 twelvetone_let = [0,1,2,3,4,5,6,7,8,9,'e','t']
 build = range(12)[1:]
-
-while(1):
-	string_po = raw_input('Enter your row (no spaces, use t and e for 10 and 11): ')
-	try:
-		po = tt_parse(string_po, 12, 10, 11)
-		if(sorted(po) != twelvetone):
-			print("ERROR: INVALID ROW - REPEATED PITCH")
-			continue
-	except NameError:
-		continue
-	break
-			
-#generate matrix
-sub_po = po[:]
-matrix = [sub_po,[],[],[],[],[],[],[],[],[],[],[]]
-
-for i in build:
-	inter = []
-	flag_a = 0
-	for j in range(12):
-		inter.append((sub_po[j] + i) % 12)
-		if(((sub_po[j] + i ) % 12) == sub_po[0]):
-			flag_a = j
-			matrix[flag_a] = inter
-		
-for i in range(12):
-	for j in range(12):
-		if(matrix[i][j] == 10):
-			matrix[i][j] = 't'
-		if(matrix[i][j] == 11):
-			matrix[i][j] = 'e'
+string_po = raw_input('Enter your row (no spaces, use t and e for 10 and 11): ')
+try:
+	matrix = matrix_builder(string_po)
+except ValueError:
+	print("ERROR")
+	quit()
+except NameError:
+	print("ERROR")
+	quit()
 
 print("Twelve Tone Matrix:")
 for i in range(12):
@@ -56,6 +35,29 @@ while(1):
 			newhexachord = tt_parse(newhexachord_inp, 6, 't', 'e')
 			hexachordsearch(newhexachord, "instances", matrix)
 			continue
+		else:
+			print("ERROR: INVALID INPUT")
+			continue
+	except ValueError:
+		print("ERROR: INVALID INPUT")
+		continue
+
+while(1):
+	do_continue = raw_input('Search for common hexachords in another matrix? (y/n): ')
+	try:
+		do_continue_str = str(do_continue)
+		if( do_continue_str == 'n' or do_continue_str == 'N'):
+			break
+		if( do_continue_str == 'y' or do_continue_str == 'Y'):
+			new_tonerow = newhexachord_inp = raw_input('Enter your row (no spaces, use t and e for 10 and 11): ')
+			new_matrix = matrix_builder(new_tonerow)
+			print("Twelve Tone Matrix:")
+			for i in range(12):
+				print("[" + ",".join( str(x) for x in new_matrix[i]) + "]")
+			originalmatrix_hexachord_list = hexachordlist(matrix)
+			for i in originalmatrix_hexachord_list:
+				hexachordsearch(i, "instances", new_matrix, printno = 0)
+			break
 		else:
 			print("ERROR: INVALID INPUT")
 			continue
--- matrix_tools.py
+++ matrix_tools.py
@@ -1,27 +1,27 @@
 # (c) Brian Voyer 2016
 # Module to provide functions for matrix
 def tt_parse(inputstring, numexpected, ten_rep, eleven_rep):
-	flag = 0
-	po_int=list(inputstring)
-	if(len(po_int) != numexpected):
+	_flag = 0
+	_po_int=list(inputstring)
+	if(len(_po_int) != numexpected):
 		print("ERROR: INVALID ROW - NOT " + str(numexpected) + " TONES")
 		raise NameError()
 	for i in range(numexpected):
-		if(po_int[i] == 't' or po_int[i] == 'T'):
-			po_int[i] = ten_rep
-		elif(po_int[i] == 'e' or po_int[i] == 'E'):
-			po_int[i] = eleven_rep
+		if(_po_int[i] == 't' or _po_int[i] == 'T'):
+			_po_int[i] = ten_rep
+		elif(_po_int[i] == 'e' or _po_int[i] == 'E'):
+			_po_int[i] = eleven_rep
 		else:
 			try:
-				po_int[i] = int(po_int[i])
+				_po_int[i] = int(_po_int[i])
 			except ValueError:
 				print("ERROR: INVALID INPUT")
 				raise NameError()
-	return(po_int)
+	return(_po_int)
 
 
 
-def hexachordsearch(hexachord, type_inp, matrix_inp):
+def hexachordsearch(hexachord, type_inp, matrix_inp, printno = 1):
 	if( len(hexachord) != 6 ):
 		raise ValueError()
 	hc_type = str(type_inp)
@@ -52,6 +52,58 @@ def hexachordsearch(hexachord, type_inp, matrix_inp):
 						print("I"+str(matrix_inp[0][j])+": "+"["+",".join( str(x) for x in hex_int)+"]")
 					else:
 						print("I"+str(matrix_inp[0][j])+": "+"["+",".join( str(x) for x in hex_int)+"]")
-	if(flag == 0):
+	if(flag == 0 and printno == 1):
 		print("No " + hc_type + " of " + pp_hexachord + " found.")
 
+def matrix_builder(tonerow):
+	_twelvetone = [0,1,2,3,4,5,6,7,8,9,10,11]
+	_build = range(12)[1:]
+	try:
+		_po = tt_parse(tonerow, 12, 10, 11)
+	except NameError:
+		print("TTPARSE ERROR")
+		return(0)
+	if(sorted(_po) != _twelvetone):
+		print("ERROR: INVALID ROW - REPEATED PITCH")
+		raise NameError()
+	_sub_po = _po[:]
+	_matrix = [_sub_po,[],[],[],[],[],[],[],[],[],[],[]]
+	for i in _build:
+		_inter = []
+		_flag_a = 0
+		for j in range(12):
+			_inter.append((_sub_po[j] + i) % 12)
+			if(((_sub_po[j] + i ) % 12) == _sub_po[0]):
+				_flag_a = j
+				_matrix[_flag_a] = _inter
+	for i in range(12):
+		for j in range(12):
+			if(_matrix[i][j] == 10):
+				_matrix[i][j] = 't'
+			if(_matrix[i][j] == 11):
+				_matrix[i][j] = 'e'
+	return(_matrix)
+
+def hexachordlist(matrix_inp):
+	hex_int = []
+	hexachord_list = []
+	for i in range(7)[:]:
+		for j in range(12):
+			hex_int = matrix_inp[i][j:j+6]
+			if( len(hex_int) == 6 ):
+				hexachord_list.append(hex_int)
+			hex_int = []
+			for k in range(6):
+				hex_int.append(matrix_inp[i:i+6][k][j])
+			if( len(hex_int) == 6 ):
+				hexachord_list.append(hex_int)
+	return(hexachord_list)
+
+def to_pitches(tonerow, fundamental):
+	pitches = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+	basis_pitch_ = pitches.index(fundamental)
+	internal = []
+	for i in tonerow:
+		__int_ = pitches[(basis_pitch_ + int(tt_parse(i,1,10,11)[0]))%12]
+		internal.append(__int_)
+	return(internal)
